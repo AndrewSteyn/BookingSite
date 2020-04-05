17deaef9352539057f87081316d0d3cd36fbdbef
AndrewSteyn <andrewpvdrsteyn@gamil.com>
2020-04-05 20:51:11 +0000

    file cleanup 2

--- KIMPTON-Hotel Bookings/romeoindex.php
+++ KIMPTON-Hotel Bookings/romeoindex.php
@@ -13,6 +13,7 @@ session_start();
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <!--CSS link-->
+    <link rel="stylesheet" href="jquery.datetimepicker.min.css" />
     <link rel="stylesheet" type="text/css" href="css/style.css">
 
     <title>KIMPTON Hotel Booking</title>
@@ -333,6 +334,8 @@ session_start();
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="jquery.js"></script>
+    <script src="jquery.datetimepicker.full.js"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
--- script.js
+++
@@ -1,8 +0,0 @@
-
-//Jqwery Ripples effect js script
-<script>
-    $('#header').ripples({
-      resolution:400,
-      perturbance:0.4,
-    });
-  </script>
